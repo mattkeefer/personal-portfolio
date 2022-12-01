74716ac8a9c8d5ffef9dcf709058121e44106162
Matt Keefer <51294591+mattkeefer@users.noreply.github.com>
2022-12-01 16:00:52 -0500

    Update icons and images

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     <header className="bg-gray-800 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="title-font font-medium text-white mb-4 md:mb-0 hover:text-red-500">
-          <a href="#about" className="ml-3 text-xl">
+          <a href="https://mattkeefer.github.io" className="ml-3 text-xl">
             Matt Keefer
           </a>
         </a>
